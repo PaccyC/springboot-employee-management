9dd092762db57e2457e02583e71e65293149da62
PaccyC <abayisengaaimepacifique@gmail.com>
2024-06-20 10:26:17 +0200

    initial commit

---
+++ src/main/java/rw/ac/app/employeeservice/entities/Employee.java
@@ -0,0 +1,29 @@
+package rw.ac.app.employeeservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String institution;
+    private String position;
+
+
+}
